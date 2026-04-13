c4a054dd9f391dbb97eb4aeefb198c85fdf55d8c
IsaacAneek <81284474+IsaacAneek@users.noreply.github.com>
2026-04-13 21:29:58 +0600

    Implemented recursion call depth simulation

--- avrstkchk.lua
+++ avrstkchk.lua
@@ -16,7 +16,11 @@ local function parse_call_instruction(line)
 	if call then
 		local callee_func_name = line:match("0x%x+%s+<([%w_]+)>")
 		if FunctionCallGraph[current_function] then
-			table.insert(FunctionCallGraph[current_function], callee_func_name)
+			table.insert(FunctionCallGraph[current_function].calls, callee_func_name)
+			-- check recursion
+			if current_function == callee_func_name then
+				FunctionCallGraph[current_function].is_recursive = true
+			end
 		end
 	end
 end
@@ -50,7 +54,7 @@ local function parse_stack_frame_pointer_adjustment(line)
 	end
 end
 
-function AnalyzeAsmFile(asm_file)
+function GetFunctionInfoFromASMFile(asm_file)
 	for line in asm_file:lines() do
 		parse_stack_frame_pointer_adjustment(line)
 		parse_call_instruction(line)
@@ -64,8 +68,28 @@ function AnalyzeAsmFile(asm_file)
 			FunctionStackUsage[current_function] = stack_usage_current_function
 			stack_usage_current_function = 2
 			hex_str = ""
-			FunctionCallGraph[new_func_name] = {}
+			FunctionCallGraph[new_func_name] = {
+				calls = {},          -- contains callee function names
+				is_recursive = false,
+				max_recursion_depth = 10 --default recursion depth 10
+			}
 			current_function = new_func_name
+		end
+	end
+end
+
+function SetRecursionDepth(function_name, recursion_depth)
+	if FunctionCallGraph[function_name].is_recursive then
+		if recursion_depth then
+			FunctionCallGraph[function_name].max_recursion_depth = recursion_depth
+		end
+	end
+end
+
+function SimulateRecursionDepth()
+	for func_name, func_info_object in pairs(FunctionCallGraph) do
+		if FunctionStackUsage[func_name] and func_info_object.is_recursive then
+			FunctionStackUsage[func_name] = FunctionStackUsage[func_name] * func_info_object.max_recursion_depth
 		end
 	end
 end
@@ -85,9 +109,9 @@ local function print_stack_usage()
 end
 
 local function print_call_graph()
-	for key, arr in pairs(FunctionCallGraph) do
+	for key, obj in pairs(FunctionCallGraph) do
 		io.write(key .. " {")
-		for _, val in ipairs(arr) do
+		for _, val in ipairs(obj.calls) do
 			io.write(val .. ", ")
 		end
 		io.write(" }\n")
@@ -96,7 +120,8 @@ end
 
 local asm_file = OpenAsmFile(arg[1])
 if asm_file then
-	AnalyzeAsmFile(asm_file)
+	GetFunctionInfoFromASMFile(asm_file)
+	SimulateRecursionDepth()
 	print_stack_usage()
 	print_call_graph()
 	asm_file:close()
